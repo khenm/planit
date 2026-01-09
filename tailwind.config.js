454d3f14d45e64ec8522e6122b845efc1dd9d6c9
Mai Phuong <phuong.tranolive@hcmut.edu.vn>
2026-01-09 17:26:40 +0700

    style: refine global styles and tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,15 @@ export default {
       colors: {
         slate: colors.slate,
         sky: colors.sky,
+        brand: {
+          dark: '#333333',
+          gray: '#b0bec5',
+          primary: '#5f7c8c',
+          white: '#ffffff',
+        }
       },
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['"Bricolage Grotesque"', 'sans-serif'],
       },
     },
   },
